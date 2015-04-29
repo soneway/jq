2bf0f4cae5951ed857d81fd13b9ea9a9cf66a6d9
soneway <250084439@qq.com>
2015-04-29 12:56:08 +0800

    生成页面

---
+++ plugin/customalert.js
@@ -0,0 +1,105 @@
+﻿//$.customalert
+(function (window, $) {
+
+    //1.变量
+    var $title, $content,
+        $btnOk, $btnCancel,
+        btnOkClick, btnCancelClick,
+        onShow, onHide,
+        document = window.document,
+        $doc = $(document),
+        $customalert = $('#customalert');
+
+
+    //2.初始化
+    (function () {
+        if ($customalert.length === 0) {
+            $customalert = $('<div id="customalert">' +
+            '<div>' +
+            '<div class="box">' +
+            '<h1 id="customalert-title">提示</h1>' +
+            '<p id="customalert-content">是否转到登陆</p>' +
+            '<a id="customalert-btnok" class="btn">确定</a>' +
+            '<a id="customalert-btncancel" class="btn">关闭</a>' +
+            '</div>' +
+            '</div>' +
+            '</div>');
+            //添加html元素
+            $(document.body).append($customalert);
+        }
+
+        $title = $('#customalert-title');
+        $content = $('#customalert-content');
+        $btnOk = $('#customalert-btnok');
+        $btnCancel = $('#customalert-btncancel');
+    })();
+
+
+    //3.事件
+    //确定按钮
+    $doc.on('click', '#customalert-btnok', function () {
+        //隐藏
+        $customalert.removeClass('visible');
+        typeof onHide === 'function' && onHide();
+        typeof btnOkClick == 'function' && btnOkClick();
+    });
+    //关闭按钮
+    $doc.on('click', '#customalert-btncancel', function () {
+        //隐藏
+        $customalert.removeClass('visible');
+        typeof onHide === 'function' && onHide();
+        typeof btnCancelClick === 'function' && btnCancelClick();
+    });
+
+
+    //4.扩展属性
+    $.customalert = function (options) {
+        options = options || {};
+
+        //配置项
+        var opts = $.extend({}, $.customalert.defaults, options);
+
+        var title = opts.title,
+            content = opts.content,
+            btnOkText = opts.btnOkText,
+            btnCancelText = opts.btnCancelText,
+            isAlert = opts.isAlert;
+
+        btnOkClick = opts.btnOkClick;
+        btnCancelClick = opts.btnCancelClick;
+        onShow = opts.onShow;
+        onHide = opts.onHide;
+
+        //是否是alert(只显示确定按钮)
+        if (isAlert) {
+            $customalert.addClass('alert');
+        }
+        else {
+            $customalert.removeClass('alert');
+        }
+
+        //设置内容
+        title && $title.html(title);
+        content && $content.html(content);
+        btnOkText && $btnOk.text(btnOkText);
+        btnCancelText && $btnCancel.text(btnCancelText);
+
+        //显示
+        $customalert.addClass('visible');
+        typeof onShow === 'function' && onShow();
+    };
+
+    //默认配置
+    $.customalert.defaults = {
+        title: '提示',
+        content: '内容',
+        btnOkText: '确定',
+        btnOkClick: null,
+        btnCancelText: '取消',
+        btnCancelClick: null,
+        isAlert: true,
+        onShow: null,
+        onHide: null
+    };
+
+})(this, $);
---
+++ plugin/flip.js
@@ -0,0 +1,181 @@
+﻿//flip.js
+(function (window, $) {
+
+    $.fn.flip = function (options) {
+        $.fn.flip.defaults = {
+            //是否竖直方向滚动
+            isVertical: false,
+            //滑动阈值
+            swipThreshold: 40,
+            //比率
+            rate: 1.3,
+            //轮播回调函数
+            slideCallback: null
+        };
+
+        //每个元素执行
+        return this.each(function () {
+            var opts = $.extend({}, $.fn.flip.defaults, options);
+
+            //配置项
+            var isVertical = opts.isVertical,
+                swipThreshold = opts.swipThreshold,
+                rate = opts.rate,
+                slideCallback = opts.slideCallback;
+
+            //变量
+            var $this = $(this),
+                $items = $this.children('*');
+
+            //初始化函数
+            function init() {
+                $this.addClass('pi-flip');
+
+                //初始化第一个item显示
+                $items.eq(0).addClass('visible');
+                //滚动回调函数
+                typeof slideCallback === 'function' && slideCallback(0);
+
+                //初始化事件
+                initEvent();
+            }
+
+            //初始化事件函数
+            function initEvent() {
+                var itemCount = $items.length,
+                    index = 0,
+                    duration = parseFloat($items.css('transition-duration') || $items.css('-webkit-transition-duration')) * 1000,
+                    startX, startY,
+                    swipSpan, isAnimating;
+
+                //复位函数
+                function reset(me) {
+                    me.style.cssText = '';
+                }
+
+                //旋转到函数
+                function rotate(swipSpan) {
+                    var transform;
+
+                    if (typeof swipSpan === 'number') {
+                        $items.each(function (i) {
+                            var $this = $(this);
+                            if (i === index) {
+                                transform = isVertical ? 'rotate3d(1,0,0,' + -swipSpan + 'deg)' : 'rotate3d(0,1,0,' + swipSpan + 'deg)';
+                                $this.css({
+                                    '-webkit-transform': transform,
+                                    'transform': transform
+                                });
+                            }
+                            else {
+                                transform = isVertical ? 'rotate3d(1,0,0,' + (180 - swipSpan) + 'deg)' : 'rotate3d(0,1,0,' + -(180 - swipSpan) + 'deg)';
+                                $this.css({
+                                    '-webkit-transform': transform,
+                                    'transform': transform
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        isAnimating = true;
+                        //滚动回调函数
+                        $.isFunction(slideCallback) && slideCallback(index);
+
+                        $items.each(function (i) {
+                            var $this = $(this);
+                            if (i === index) {
+                                transform = isVertical ? 'rotate3d(1,0,0,' + -(swipSpan ? 0 : -360) + 'deg)' : 'rotate3d(0,1,0,' + (swipSpan ? 0 : -360) + 'deg)';
+                                $this.addClass('visible').css({
+                                    '-webkit-transform': transform,
+                                    'transform': transform
+                                });
+                            }
+                            else {
+                                transform = isVertical ? 'rotate3d(1,0,0,' + -(swipSpan ? 180 : -180) + 'deg)' : 'rotate3d(0,1,0,' + (swipSpan ? 180 : -180) + 'deg)';
+                                $this.removeClass('visible').css({
+                                    '-webkit-transform': transform,
+                                    'transform': transform
+                                });
+                            }
+                        });
+
+                        //延迟复位
+                        setTimeout(function () {
+                            //加上动画
+                            $items.addClass('notrans').each(function () {
+                                reset(this);
+                            });
+                            isAnimating = false;
+                        }, duration);
+                    }
+                }
+
+                //触摸开始事件
+                $this.on('touchstart', function (evt) {
+                    if (!isAnimating) {
+                        var touch = evt.targetTouches[0];
+                        //记录触摸开始位置
+                        startX = touch.pageX;
+                        startY = touch.pageY;
+                        //重置swipSpan
+                        swipSpan = 0;
+
+                        //去掉动画
+                        $items.addClass('notrans');
+                    }
+                });
+
+                //触摸移动事件
+                $this.on('touchmove', function (evt) {
+                    var touch = evt.targetTouches[0],
+                        swipSpanX = touch.pageX - startX,
+                        swipSpanY = touch.pageY - startY;
+
+                    //上下
+                    if (isVertical && Math.abs(swipSpanX) < Math.abs(swipSpanY)) {
+                        evt.preventDefault();
+                        evt.stopPropagation();
+                        !isAnimating && rotate(swipSpan = swipSpanY / rate);
+                    }
+                    //左右
+                    if (!isVertical && Math.abs(swipSpanX) > Math.abs(swipSpanY)) {
+                        evt.preventDefault();
+                        evt.stopPropagation();
+                        !isAnimating && rotate(swipSpan = swipSpanX / rate);
+                    }
+                });
+
+                //触摸结束事件
+                $this.on('touchend', function (evt) {
+                    if (!isAnimating) {
+                        //达到滚动阈值
+                        if (Math.abs(swipSpan) > swipThreshold) {
+                            if (swipSpan > 0 && --index === -1) {
+                                index = itemCount - 1;
+                            }
+                            if (swipSpan < 0 && ++index === itemCount) {
+                                index = 0;
+                            }
+
+                            //加上动画
+                            $items.removeClass('notrans');
+                            rotate(swipSpan > 0);
+                        }
+                        else if (swipSpan !== 0) {
+                            //加上动画
+                            $items.eq(index).removeClass('notrans');
+                            reset($items[index]);
+                        }
+                    }
+                });
+            }
+
+
+            //初始化
+            init();
+
+        });
+
+    };
+
+})(this, $);
