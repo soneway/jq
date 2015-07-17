bf3838cd9b821665185c96cd03a16efa96d4e2b0
soneway <250084439@qq.com>
2015-07-17 18:49:42 +0800

    优化spinner动画和焦点图等插件

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,8 +1,8 @@
-//npm install gulp gulp-sass gulp-minify-css gulp-browserify gulp-uglify --save-dev
+//npm install gulp gulp-sass gulp-minify-css gulp-browserify gulp-uglify gulp-imagemin imagemin-pngquant gulp-htmlmin --save-dev
 'use strict';
 
 //输出文件夹
-var dest = './out';
+var out = './out/';
 //配置对象
 var config = {
     css: {
@@ -11,14 +11,14 @@ var config = {
         //监听文件
         watch: ['./css/**'],
         //输出文件夹
-        dest: dest,
+        dest: out + 'css',
         //是否压缩
         isPack: 1
     },
     js: {
         src: ['./js/*.js'],
         watch: ['./js/**'],
-        dest: dest,
+        dest: out + 'js',
         isPack: 1,
         //模块化js文件shim
         shim: {
@@ -28,6 +28,17 @@ var config = {
                 exports: '$'
             }
         }
+    },
+    images: {
+        src: ['./images/**'],
+        watch: ['./images/**'],
+        dest: out + 'images'
+    },
+    html: {
+        src: ['*.html'],
+        watch: ['*.html'],
+        dest: out,
+        isPack: 0
     }
 };
 
@@ -40,17 +51,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 gulp.task('css', function () {
-    var css = config.css;
+    var conf = config.css;
 
-    var task = gulp.src(css.src)
+    var task = gulp.src(conf.src)
         //编译
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(css.dest));
+        .pipe(gulp.dest(conf.dest));
 
     //压缩
-    if (css.isPack) {
+    if (conf.isPack) {
         task.pipe(minifyCss())
-            .pipe(gulp.dest(css.dest));
+            .pipe(gulp.dest(conf.dest));
     }
 });
 
@@ -59,19 +70,52 @@ gulp.task('css', function () {
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 gulp.task('js', function () {
-    var js = config.js;
+    var conf = config.js;
 
-    var task = gulp.src(js.src)
+    var task = gulp.src(conf.src)
         //编译合并
         .pipe(browserify({
-            shim: js.shim
+            shim: conf.shim
         }))
-        .pipe(gulp.dest(js.dest));
+        .pipe(gulp.dest(conf.dest));
 
     //压缩
-    if (js.isPack) {
+    if (conf.isPack) {
         task.pipe(uglify())
-            .pipe(gulp.dest(js.dest));
+            .pipe(gulp.dest(conf.dest));
+    }
+});
+
+
+//images压缩
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+gulp.task('images', function () {
+    var conf = config.images;
+
+    if (conf) {
+        gulp.src(conf.src)
+            .pipe(imagemin({
+                progressive: true,
+                svgoPlugins: [{removeViewBox: false}],
+                use: [pngquant()]
+            }))
+            .pipe(gulp.dest(conf.dest));
+    }
+});
+
+
+//html压缩
+var htmlmin = require('gulp-htmlmin');
+gulp.task('html', function () {
+    var conf = config.html;
+
+    if (conf && conf.isPack) {
+        gulp.src(conf.src)
+            .pipe(htmlmin({
+                collapseWhitespace: true
+            }))
+            .pipe(gulp.dest(conf.dest));
     }
 });
 
@@ -81,8 +125,10 @@ gulp.task('watch', function () {
     //监听文件变化
     gulp.watch(config.css.watch, ['css']);
     gulp.watch(config.js.watch, ['js']);
+    gulp.watch(config.images.watch, ['images']);
+    gulp.watch(config.html.watch, ['html']);
 });
 
 
 //默认任务
-gulp.task('default', ['watch', 'css', 'js']);
+gulp.task('default', ['watch', 'css', 'js', 'images', 'html']);
