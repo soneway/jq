703b9c3b37ab9bfb8a5e522cc26327c50ecd3248
soneway <250084439@qq.com>
2015-08-05 12:02:30 +0800

    小优化

--- example/src/js/index.js
+++ example/src/js/index.js
@@ -12,19 +12,6 @@ window.alert = function alert(str) {
 };
 
 
-//面板显示回调函数
-$.panelLoaded = function ($this, isInit) {
-    var load = loader[$this.attr('id')];
-    typeof load === 'function' && load($this, isInit);
-};
-
-//面板隐藏回调函数
-$.panelUnloaded = function ($this) {
-    var unload = (loader[$this.attr('id')] || {}).unload;
-    typeof unload === 'function' && unload($this);
-};
-
-
 //页面模块加载对象
 var loader = {
     carousel   : require('./page/carousel.js'),
@@ -34,4 +21,16 @@ var loader = {
     scratchcard: require('./page/scratchcard.js'),
     turntable  : require('./page/turntable.js'),
     swatchbook : require('./page/swatchbook.js')
+};
+
+
+//面板显示回调函数
+$.panelLoaded = function ($this, isInit) {
+    var load = loader[$this.attr('id')];
+    typeof load === 'function' && load($this, isInit);
+};
+//面板隐藏回调函数
+$.panelUnloaded = function ($this) {
+    var unload = (loader[$this.attr('id')] || {}).unload;
+    typeof unload === 'function' && unload($this);
 };
