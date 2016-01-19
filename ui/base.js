c882172644f220966a18a8d6c3d777ed5a7b8c41
soneway <250084439@qq.com>
2016-01-19 12:45:20 +0800

    cm

--- ui/base.js
+++ ui/base.js
@@ -78,7 +78,7 @@
         });
 
         //pc端二维码
-        $.isShowQrcode && !$.isMobi && $.jsonp('http://img.gd.sohu.com/static/v2/qrcode.js', function () {
+        $.isShowQrcode && !$.isMobi && $.jsonp('http://img.gd.sohu.com/static/v3/qrcode.js', function () {
                 var $qrcode = $('#qrcode');
                 if ($qrcode.length === 0) {
                     $qrcode = $('<div id="qrcode"></div>');
@@ -96,7 +96,7 @@
         );
 
         //pc端mouse转touch事件
-        !$.isMobi && $.jsonp('http://img.gd.sohu.com/static/v2/desktouch.js');
+        !$.isMobi && $.jsonp('http://img.gd.sohu.com/static/v3/desktouch.js');
 
     });
 
