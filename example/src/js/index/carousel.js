10573fcf87f4e09a80cecb40cdaadc6245022d6e
soneway <250084439@qq.com>
2016-02-05 10:16:20 +0800

    cm

--- example/src/js/index/carousel.js
+++ example/src/js/index/carousel.js
@@ -7,18 +7,17 @@
     //加载时执行
     function load($this, isInit) {
         if (isInit) {
+            var html = '';
+            for (var i = 1, len = 6; i < len; i++) {
+                html += '<img data-title="标题' + i + '" src="img/' + i + '.jpg"/>';
+            }
+
             $('.carousel').each(function () {
-                $(this).carousel({
+                $(this).html(html).carousel({
                     isVertical: this.getAttribute('data-isvertical') === '1',
                     isAutoPlay: false
                 });
             });
-
-            var html = '';
-            for (var i = 0, len = 5; i < len; i++) {
-                html += '<img src="img/' + i + '.jpg"/>';
-            }
-            $('#img_car').html(html).carousel();
         }
     }
 
