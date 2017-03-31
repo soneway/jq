401bae54047792e8a165e49b06cc0fb12e1ae602
宋均均 <sjj107294@alibaba-inc.com>
2017-03-31 21:56:38 +0800

    chore: https

--- example/dev/js/index.js
+++ example/dev/js/index.js
@@ -214,14 +214,14 @@ require('picpager');
 exports.load = function ($this, isInit) {
     if (isInit) {
         var page = 1;
-        $.getScript('http://app.gd.sohu.com/minisite/xtep/20140530/get.php?vname=rs&act=list&page=' + page + '&code=aa1c9153608a7755b7c20e97c0eade27', function () {
+        $.getScript('https://app.gd.sohu.com/minisite/xtep/20140530/get.php?vname=rs&act=list&page=' + page + '&code=aa1c9153608a7755b7c20e97c0eade27', function () {
             var $picpager = $('.picpager').picpager({
                 imgData: rs.data.detail.map(function (item) {
                     return item.image;
                 }),
                 imgAttrName: 'image',
                 slideCallback: function (index) {
-                    index + 1 === page * 10 && $.getScript('http://app.gd.sohu.com/minisite/xtep/20140530/get.php?vname=rs&act=list&page=' + ++page + '&code=aa1c9153608a7755b7c20e97c0eade27', function () {
+                    index + 1 === page * 10 && $.getScript('https://app.gd.sohu.com/minisite/xtep/20140530/get.php?vname=rs&act=list&page=' + ++page + '&code=aa1c9153608a7755b7c20e97c0eade27', function () {
                         $picpager[0].addItem(rs.data.detail.map(function (item) {
                             return item.image;
                         }));
